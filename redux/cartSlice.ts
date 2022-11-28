e83dfa102f96ec46bc413510b5da1f5bab498656
unknown <andre.lopes.melani@hotmail.com>
2022-11-27 21:26:04 -0300

    first commit

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -5,9 +5,9 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
-    addToCart: (state, action) => {
+    addToCart: (state : any, action: any) => {
       const itemExists = state.find(
-        (item) => item.id === action.payload.id
+        (item: any) => item.id === action.payload.id
       )
       if (itemExists) {
         itemExists.quantity++
@@ -16,19 +16,19 @@ const cartSlice = createSlice({
       }
     },
     incrementQuantity: (state, action) => {
-      const item = state.find(
-        (item) => item.id === action.payload
+      const item: any = state.find(
+        (item: any) => item.id === action.payload
       )
       item.quantity++
     },
 
     decrementQuantity: (state, action) => {
-      const item = state.find(
-        (item) => item.id === action.payload
+      const item: any = state.find(
+        (item: any) => item.id === action.payload
       )
       if (item.quantity === 1) {
         const index = state.findIndex(
-          (item) => item.id === action.payload
+          (item: any) => item.id === action.payload
         )
         state.splice(index, 1)
       } else {
@@ -37,7 +37,7 @@ const cartSlice = createSlice({
     },
     removeFromCart: (state, action) => {
       const index = state.findIndex(
-        (item) => item.id === action.payload
+        (item: any) => item.id === action.payload
       )
       state.splice(index, 1)
     },
